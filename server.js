b840527e89d746d6b88faaca8c6e303a06732124
In Code We Trust <njderenne@gmail.com>
2020-11-13 22:35:23 -0600

    added front end code and linked to server side js

--- server.js
+++ server.js
@@ -11,6 +11,8 @@ app.use(express.urlencoded({ extended: true}));
 //parse incoming JSON data
 app.use(express.json());
 
+app.use(express.static('public'));
+
 function filterByQuery(query, animalsArray) {
     let personalityTraitsArray = [];
     // Note that we save the animalsArray as filteredResults here:
@@ -99,6 +101,17 @@ app.get('/api/animals/:id', (req, res) => {
     }
 });
 
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/index.html'));
+});
+
+app.get('/animals', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/animals.html'));
+});
+
+app.get('/zookeepers', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/zookeepers.html'));
+});
 
 app.post('/api/animals', (req, res) => {
     //set id based on what the next index of the array will be
